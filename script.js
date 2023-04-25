e262b12ff6df5f4d9c5584491bee1228754276b6
jann <jannandrea.f@gmail.com>
2023-04-25 10:22:30 +0200

    feat: daten werden geholt, nichts angezeigt

--- script.js
+++ script.js
@@ -1,50 +1,77 @@
-function createTaskElement(task) {
-  const div = document.createElement("div");
-  div.classList.add("task");
+const inputBox = document.querySelector("#todo-inputBox");
+const form = document.querySelector("#todo-form");
+const list = document.querySelector("#todo-list");
 
-  const p = document.createElement("li");
-  p.innerText = task;
-  p.classList.add("p-task");
-  div.append(p);
+function renderTasks(data) {
+  list.replaceChildren();
+  for (let i = 0; i < data.length; i++) {
+    const todo = data[i];
+    const li = document.createElement("li");
+    li.classList.add("p.task");
+    li.textContent = todo.title;
 
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Delete";
-  deleteButton.classList.add("delete-btn");
-  div.append(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-btn");
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+    });
 
-  const editButton = document.createElement("button");
-  editButton.innerText = "Edit";
-  editButton.classList.add("edit-btn");
-  div.append(editButton);
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.classList.add("edit-btn");
 
-  deleteButton.addEventListener("click", function () {
-    div.remove();
-  });
+    editButton.addEventListener("click", function () {
+      const newTask = prompt("Edit task", li.innerText);
 
-  editButton.addEventListener("click", function () {
-    const newTask = prompt("Edit task", p.innerText);
+      if (newTask === null || newTask.trim() === "") {
+        return;
+      }
 
-    if (newTask === null || newTask.trim() === "") {
-      return;
-    }
+      li.innerText = newTask;
+    });
 
-    p.innerText = newTask;
-  });
+    const buttonDiv = document.createElement("div");
+    buttonDiv.appendChild(deleteButton);
+    buttonDiv.appendChild(editButton);
 
-  return div;
-}
+    const taskDiv = document.createElement("div");
+    taskDiv.appendChild(li);
+    taskDiv.appendChild(buttonDiv);
 
-function addTask() {
-  const task = document.getElementById("todo-inputBox").value;
-
-  if (task.trim() === "") {
-    return;
+    list.appendChild(taskDiv);
   }
-
-  const div = createTaskElement(task);
-
-  const container = document.getElementById("todo-list");
-  container.append(div);
-
-  document.getElementById("todo-inputBox").value = "";
 }
+
+
+async function getTasks() {
+  const response = await fetch("http://127.0.0.1:5000/todo");
+  const data = await response.json();
+  console.log(data); // Debugging purposes
+
+  renderTasks(data);
+}
+
+
+async function addTask(event) {
+  event.preventDefault();
+  const title = inputBox.value;
+
+  const response = await fetch("http://127.0.0.1:5000/todo", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: title }),
+  });
+
+  const data = await response.json();
+
+  inputBox.value = "";
+
+  renderTasks([data]);
+  getTasks();
+
+  alert("Task successfully added!");
+}
+
+getTasks();
+
